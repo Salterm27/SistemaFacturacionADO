8570d12581316dd1716d861e35028fecaacc9e47
MarcoJM <marcomerlos@hotmail.com>
2021-06-17 00:20:47 -0300

    Nuevas Pantallas Producto

---
+++ src/main/java/domain/vista/AsociacionProducto.java
@@ -0,0 +1,14 @@
+package domain.vista;
+
+import javax.swing.*;
+
+public class AsociacionProducto {
+    private JPanel panelPrincipal;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JTextField textField1;
+    private JComboBox comboBox3;
+    private JTextField textField2;
+    private JButton asociarProductoButton;
+    private JButton cancelarButton;
+}
---
+++ src/main/java/domain/vista/CreacionProducto.java
@@ -0,0 +1,12 @@
+package domain.vista;
+
+import javax.swing.*;
+
+public class CreacionProducto {
+    private JPanel panelPrincipal;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JButton asociarPoductoYProveedorButton;
+    private JButton aceptarButton;
+    private JButton cancelarButton;
+}
