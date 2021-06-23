4581f97a85f19e3bc7202f0f6d658ecde248ade0
MarcoJM <marcomerlos@hotmail.com>
2021-06-23 19:04:18 -0300

    Ajustes Vistas

--- src/main/java/domain/vista/ConsultasGenerales.java
+++ src/main/java/domain/vista/ConsultasGenerales.java
@@ -47,10 +47,11 @@ public class ConsultasGenerales {
         JFrame frame = new JFrame("Consultas Generales");
         frame.setContentPane( new ConsultasGenerales().panelPrincipal);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.pack();
         frame.setVisible(true);
-        frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+                frame.setResizable(false);
+
 
     }
 }
